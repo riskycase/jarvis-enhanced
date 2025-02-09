1fc8cbe90072d33ccd25772ef45fc0720adabb89
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2025-02-09 20:30:20 +0530

    Move all clock drawing to own class

--- app/src/main/java/com/riskycase/jarvisEnhanced/service/WallpaperService.kt
+++ app/src/main/java/com/riskycase/jarvisEnhanced/service/WallpaperService.kt
@@ -6,15 +6,8 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Matrix
 import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.PointF
 import android.graphics.RectF
-import android.graphics.SweepGradient
-import android.icu.text.SimpleDateFormat
-import android.icu.util.Calendar
-import android.os.BatteryManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -23,47 +16,27 @@ import android.provider.AlarmClock
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 import com.riskycase.jarvisEnhanced.util.wallpaper.BackgroundImageUtils
-import com.riskycase.jarvisEnhanced.util.wallpaper.ClockConstants
+import com.riskycase.jarvisEnhanced.util.wallpaper.ClockUtils
 import com.riskycase.jarvisEnhanced.util.wallpaper.MediaUtils
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-import javax.inject.Named
-import kotlin.math.PI
-import kotlin.math.cos
 import kotlin.math.max
 import kotlin.math.pow
-import kotlin.math.sin
-import kotlin.math.sqrt
 
 @AndroidEntryPoint
 class WallpaperService : WallpaperService() {
 
     @Inject
-    lateinit var batteryManager: BatteryManager
-
-    @Inject
     lateinit var keyguardManager: KeyguardManager
 
     @Inject
-    lateinit var clockConstants: ClockConstants
+    lateinit var clockUtils: ClockUtils
 
     @Inject
     lateinit var mediaUtils: MediaUtils
 
     @Inject
     lateinit var backgroundImageUtils: BackgroundImageUtils
-
-    @Inject
-    @Named("OddDateFormat")
-    lateinit var oddSimpleDateFormat: SimpleDateFormat
-
-    @Inject
-    @Named("EvenDateFormat")
-    lateinit var evenSimpleDateFormat: SimpleDateFormat
-
-    @Inject
-    @Named("7SegmentAllOnString")
-    lateinit var allOnString: String
 
     override fun onCreateEngine(): Engine {
         return MyWallpaperEngine()
@@ -85,7 +58,7 @@ class WallpaperService : WallpaperService() {
 
         override fun onVisibilityChanged(visible: Boolean) {
             this.visible = visible
-            clockConstants.updateVisibility(visible)
+            clockUtils.updateVisibility(visible)
             if (visible) {
                 handler.post(drawRunner)
             } else {
@@ -104,7 +77,7 @@ class WallpaperService : WallpaperService() {
         ) {
             this.width = width
             this.height = height
-            clockConstants.updateDimensions(width, height)
+            clockUtils.updateDimensions(width, height)
             mediaUtils.updateDimensions(width, height)
             super.onSurfaceChanged(holder, format, width, height)
         }
@@ -127,102 +100,10 @@ class WallpaperService : WallpaperService() {
             action: String?, x: Int, y: Int, z: Int, extras: Bundle?, resultRequested: Boolean
         ): Bundle {
             if (WallpaperManager.COMMAND_TAP == action && !keyguardManager.isKeyguardLocked) {
-                val clockCenter = clockConstants.getClockCenter()
-                if (((clockCenter.x - x.toFloat()).pow(2) + (clockCenter.y - y.toFloat()).pow(2)) < (clockConstants.getClockRadius()
-                        .pow(2))
-                ) {
-                    val openAlarmIntent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
-                    openAlarmIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    applicationContext.startActivity(openAlarmIntent)
-                }
+                clockUtils.handleTouchEvent(x, y)
                 mediaUtils.handleTouchInput(x, y)
             }
             return Bundle()
-        }
-
-        private fun drawHand(
-            canvas: Canvas,
-            center: PointF,
-            width: Float,
-            height: Float,
-            constant: Int,
-            handPaint: Paint,
-            radius: Float
-        ) {
-            val rotation = clockConstants.getRotation(constant)
-            val shadowPaint = clockConstants.getClockShadowPaint()
-            val sinValue = sin(rotation * PI / 180f).toFloat()
-            val cosValue = cos(rotation * PI / 180f).toFloat()
-
-            val start = PointF(
-                width.times(0.5f) * -cosValue, width.times(0.5f) * -sinValue
-            ) // width/2, rotation - 180
-            val heightOutwards = PointF(
-                height * sinValue, height * -cosValue
-            ) // height, rotation - 90
-            val widthCW = PointF(
-                width * cosValue, width * sinValue
-            ) // width, rotation
-
-
-            val handPath = Path()
-            handPath.moveTo(center.x, center.y)
-            handPath.rMoveTo(start.x, start.y)
-            handPath.rLineTo(heightOutwards.x, heightOutwards.y)
-            handPath.rLineTo(widthCW.x, widthCW.y)
-            handPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
-            handPath.close()
-            canvas.drawPath(handPath, handPaint)
-
-            val shadowPath = Path()
-            shadowPath.moveTo(center.x, center.y)
-            shadowPath.rMoveTo(start.x, start.y)
-            if (rotation < 45f || rotation > 315f) {
-                shadowPath.rLineTo(widthCW.x, widthCW.y)
-                shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
-                shadowPath.rLineTo(radius.times(2f), radius.times(2f))
-                shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
-                shadowPath.rLineTo(-widthCW.x, -widthCW.y)
-            } else if (rotation < 135f) {
-                shadowPath.rMoveTo(widthCW.x, widthCW.y)
-                shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
-                shadowPath.rLineTo(-widthCW.x, -widthCW.y)
-                shadowPath.rLineTo(radius.times(2f), radius.times(2f))
-                shadowPath.rLineTo(widthCW.x, widthCW.y)
-                shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
-            } else if (rotation < 215f) {
-                shadowPath.rMoveTo(widthCW.x, widthCW.y)
-                shadowPath.rMoveTo(heightOutwards.x, heightOutwards.y)
-                shadowPath.rLineTo(-widthCW.x, -widthCW.y)
-                shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
-                shadowPath.rLineTo(radius.times(2f), radius.times(2f))
-                shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
-                shadowPath.rLineTo(widthCW.x, widthCW.y)
-            } else {
-                shadowPath.rMoveTo(heightOutwards.x, heightOutwards.y)
-                shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
-                shadowPath.rLineTo(widthCW.x, widthCW.y)
-                shadowPath.rLineTo(radius.times(2f), radius.times(2f))
-                shadowPath.rLineTo(-widthCW.x, -widthCW.y)
-                shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
-            }
-            shadowPath.close()
-
-            val centerPath = Path()
-            centerPath.moveTo(center.x, center.y)
-            centerPath.rMoveTo((25f.plus(7.5f / 2)) / sqrt(2f), -(25f.plus(7.5f / 2)) / sqrt(2f))
-            centerPath.rLineTo(radius * 2f, radius * 2f)
-            centerPath.rLineTo(-(25f.plus(7.5f / 2)) * sqrt(2f), (25f.plus(7.5f / 2)) * sqrt(2f))
-            centerPath.rLineTo(-radius * 2f, -radius * 2f)
-            centerPath.close()
-            shadowPath.op(centerPath, Path.Op.UNION)
-
-            val clockFacePath = Path()
-            clockFacePath.addCircle(center.x, center.y, radius, Path.Direction.CW)
-            shadowPath.op(clockFacePath, Path.Op.INTERSECT)
-
-            canvas.drawPath(shadowPath, shadowPaint)
-
         }
 
         private fun drawImageCover(
@@ -243,136 +124,11 @@ class WallpaperService : WallpaperService() {
             canvas.drawBitmap(bitmap, null, rect, null)
         }
 
-        private fun drawClock(canvas: Canvas) {
-
-            val center = clockConstants.getClockCenter()
-            val radius = clockConstants.getClockRadius()
-
-            val batteryPaint = Paint()
-            batteryPaint.shader = SweepGradient(center.x, center.y, Color.RED, Color.GREEN).apply {
-                val rotationMatrix = Matrix()
-                rotationMatrix.preRotate(-90f, center.x, center.y)
-                setLocalMatrix(rotationMatrix)
-            }
-            batteryPaint.style = Paint.Style.STROKE
-            batteryPaint.strokeWidth = 20f
-
-            canvas.drawArc(
-                RectF(
-                    center.x - radius, center.y - radius, center.x + radius, center.y + radius
-                ),
-                -90f,
-                (batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY) * 3.6f),
-                false,
-                batteryPaint
-            )
-
-            val bodyPaint = Paint()
-            bodyPaint.color = backgroundImageUtils.getBaseColor()
-            bodyPaint.style = Paint.Style.FILL
-
-            // Main face
-            canvas.drawCircle(center.x, center.y, radius.minus(7.5f), bodyPaint)
-
-            bodyPaint.style = Paint.Style.STROKE
-            bodyPaint.strokeWidth = 10f
-            // Border outline
-            canvas.drawCircle(center.x, center.y, radius.plus(7.5f), bodyPaint)
-
-            val now = Calendar.getInstance()
-            val contrastColor = backgroundImageUtils.getColorOnBaseColor()
-
-            if (!keyguardManager.isKeyguardLocked) {
-                val clockTextPaint = clockConstants.getClockTextPaint()
-                val currentTimeText =
-                    (if (now.get(Calendar.SECOND) % 2 == 0) evenSimpleDateFormat else oddSimpleDateFormat).format(
-                        now
-                    )
-                val textBounds = clockConstants.getTextBounds()
-                clockTextPaint.color = backgroundImageUtils.getDarkerBaseColor()
-                canvas.drawRect(
-                    RectF(
-                        center.x - (textBounds.width().toFloat() / 2f) - 20f,
-                        center.y - (radius / 2f) - (textBounds.height().toFloat()) + 10f,
-                        center.x + (textBounds.width().toFloat() / 2f) + 17.5f,
-                        center.y - (radius / 2f) + 47.5f
-                    ), clockTextPaint
-                )
-                clockTextPaint.color = backgroundImageUtils.getDarkBaseColor()
-                canvas.drawRect(
-                    RectF(
-                        center.x - (textBounds.width().toFloat() / 2f) - 10f,
-                        center.y - (radius / 2f) - (textBounds.height().toFloat()) + 20f,
-                        center.x + (textBounds.width().toFloat() / 2f) + 10f,
-                        center.y - (radius / 2f) + 40f
-                    ), clockTextPaint
-                )
-                clockTextPaint.color = Color.argb(96, 128, 128, 128)
-                canvas.drawText(
-                    allOnString,
-                    center.x - (textBounds.width().toFloat() / 2f),
-                    center.y - (radius / 2f) + 30f,
-                    clockTextPaint
-                )
-                clockTextPaint.color = contrastColor
-                canvas.drawText(
-                    currentTimeText,
-                    center.x - (textBounds.width().toFloat() / 2f),
-                    center.y - (radius / 2f) + 30f,
-                    clockTextPaint
-                )
-            }
-
-            val clockPaint = Paint()
-            clockPaint.color = contrastColor
-            clockPaint.style = Paint.Style.FILL
-
-            // Hours hand
-            drawHand(
-                canvas,
-                center,
-                15f,
-                radius.times(0.65f),
-                Calendar.HOUR,
-                clockPaint,
-                radius.minus(7.5f)
-            )
-
-            // Minutes hand
-            drawHand(
-                canvas,
-                center,
-                10f,
-                radius.minus(7.5f),
-                Calendar.MINUTE,
-                clockPaint,
-                radius.minus(7.5f)
-            )
-
-            // Seconds hand
-            drawHand(
-                canvas,
-                center,
-                4f,
-                radius.minus(7.5f),
-                Calendar.SECOND,
-                clockPaint,
-                radius.minus(7.5f)
-            )
-
-            // Center piece
-            bodyPaint.style = Paint.Style.FILL
-            canvas.drawCircle(center.x, center.y, 25f, bodyPaint)
-            clockPaint.style = Paint.Style.STROKE
-            clockPaint.strokeWidth = 7.5f
-            canvas.drawCircle(center.x, center.y, 25f, clockPaint)
-        }
-
         private fun draw() {
             val nextDraw = SystemClock.uptimeMillis() + (1000 / 30)
             val holder = surfaceHolder
             val canvas: Canvas?
-            clockConstants.updateVisibility(visible)
+            clockUtils.updateVisibility(visible)
             if (visible) {
                 canvas = holder.lockHardwareCanvas()
                 if (canvas != null) {
@@ -391,7 +147,7 @@ class WallpaperService : WallpaperService() {
                         )
                     }
                     mediaUtils.drawMediaPlayer(canvas)
-                    drawClock(canvas)
+                    clockUtils.drawClock(canvas)
                     holder.unlockCanvasAndPost(canvas)
                 }
             }
--- app/src/main/java/com/riskycase/jarvisEnhanced/util/wallpaper/ClockUtils.kt
+++ app/src/main/java/com/riskycase/jarvisEnhanced/util/wallpaper/ClockUtils.kt
@@ -2,38 +2,57 @@ package com.riskycase.jarvisEnhanced.util.wallpaper
 
 import android.app.KeyguardManager
 import android.content.Context
+import android.content.Intent
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Matrix
 import android.graphics.Paint
+import android.graphics.Path
 import android.graphics.PointF
 import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.SweepGradient
+import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
+import android.os.BatteryManager
+import android.provider.AlarmClock
 import com.riskycase.jarvisEnhanced.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 @Singleton
-class ClockConstants @Inject constructor(
+class ClockUtils @Inject constructor(
     private val keyguardManager: KeyguardManager,
-    @ApplicationContext applicationContext: Context
+    @ApplicationContext private val applicationContext: Context
 ) {
 
     enum class ClockCenterStates {
-        KEYGUARD_LOCKED,
-        KEYGUARD_UNLOCKED
+        KEYGUARD_LOCKED, KEYGUARD_UNLOCKED
     }
 
     enum class ClockRadiusStates {
-        KEYGUARD_LOCKED,
-        KEYGUARD_UNLOCKED,
-        NOT_VISIBLE
+        KEYGUARD_LOCKED, KEYGUARD_UNLOCKED, NOT_VISIBLE
     }
 
     private var width = 0
     private var height = 0
     private var textPaint = Paint()
+        get() {
+            field.textSize = clockRadiusAnimator.getValue(System.currentTimeMillis()) / 6f
+            return field
+        }
     private var textBounds = Rect()
+        get() {
+            textPaint.getTextBounds(allOnString, 0, 8, field)
+            return field
+        }
     private val clockShadowPaint = Paint()
 
     private var previousCenterState =
@@ -57,6 +76,20 @@ class ClockConstants @Inject constructor(
     private val lockUnlockAnimationDuration = 1000L
     private val clockVisibilityAnimationDuration = 1000L
     private val clockSweepAnimationDuration = 1000L
+
+    @Inject
+    lateinit var batteryManager: BatteryManager
+
+    @Inject
+    lateinit var backgroundImageUtils: BackgroundImageUtils
+
+    @Inject
+    @Named("OddDateFormat")
+    lateinit var oddSimpleDateFormat: SimpleDateFormat
+
+    @Inject
+    @Named("EvenDateFormat")
+    lateinit var evenSimpleDateFormat: SimpleDateFormat
 
     @Inject
     @Named("7SegmentAllOnString")
@@ -243,18 +276,204 @@ class ClockConstants @Inject constructor(
         }).getValue(System.currentTimeMillis(), finalRotation)
     }
 
-    fun getClockTextPaint(): Paint {
-        textPaint.textSize = clockRadiusAnimator.getValue(System.currentTimeMillis()) / 6f
-        return textPaint
+    private fun drawHand(
+        canvas: Canvas,
+        center: PointF,
+        width: Float,
+        height: Float,
+        constant: Int,
+        handPaint: Paint,
+        radius: Float
+    ) {
+        val rotation = getRotation(constant)
+        val shadowPaint = clockShadowPaint
+        val sinValue = sin(rotation * PI / 180f).toFloat()
+        val cosValue = cos(rotation * PI / 180f).toFloat()
+
+        val start = PointF(
+            width.times(0.5f) * -cosValue, width.times(0.5f) * -sinValue
+        ) // width/2, rotation - 180
+        val heightOutwards = PointF(
+            height * sinValue, height * -cosValue
+        ) // height, rotation - 90
+        val widthCW = PointF(
+            width * cosValue, width * sinValue
+        ) // width, rotation
+
+
+        val handPath = Path()
+        handPath.moveTo(center.x, center.y)
+        handPath.rMoveTo(start.x, start.y)
+        handPath.rLineTo(heightOutwards.x, heightOutwards.y)
+        handPath.rLineTo(widthCW.x, widthCW.y)
+        handPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
+        handPath.close()
+        canvas.drawPath(handPath, handPaint)
+
+        val shadowPath = Path()
+        shadowPath.moveTo(center.x, center.y)
+        shadowPath.rMoveTo(start.x, start.y)
+        if (rotation < 45f || rotation > 315f) {
+            shadowPath.rLineTo(widthCW.x, widthCW.y)
+            shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
+            shadowPath.rLineTo(radius.times(2f), radius.times(2f))
+            shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
+            shadowPath.rLineTo(-widthCW.x, -widthCW.y)
+        } else if (rotation < 135f) {
+            shadowPath.rMoveTo(widthCW.x, widthCW.y)
+            shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
+            shadowPath.rLineTo(-widthCW.x, -widthCW.y)
+            shadowPath.rLineTo(radius.times(2f), radius.times(2f))
+            shadowPath.rLineTo(widthCW.x, widthCW.y)
+            shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
+        } else if (rotation < 215f) {
+            shadowPath.rMoveTo(widthCW.x, widthCW.y)
+            shadowPath.rMoveTo(heightOutwards.x, heightOutwards.y)
+            shadowPath.rLineTo(-widthCW.x, -widthCW.y)
+            shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
+            shadowPath.rLineTo(radius.times(2f), radius.times(2f))
+            shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
+            shadowPath.rLineTo(widthCW.x, widthCW.y)
+        } else {
+            shadowPath.rMoveTo(heightOutwards.x, heightOutwards.y)
+            shadowPath.rLineTo(-heightOutwards.x, -heightOutwards.y)
+            shadowPath.rLineTo(widthCW.x, widthCW.y)
+            shadowPath.rLineTo(radius.times(2f), radius.times(2f))
+            shadowPath.rLineTo(-widthCW.x, -widthCW.y)
+            shadowPath.rLineTo(heightOutwards.x, heightOutwards.y)
+        }
+        shadowPath.close()
+
+        val centerPath = Path()
+        centerPath.moveTo(center.x, center.y)
+        centerPath.rMoveTo((25f.plus(7.5f / 2)) / sqrt(2f), -(25f.plus(7.5f / 2)) / sqrt(2f))
+        centerPath.rLineTo(radius * 2f, radius * 2f)
+        centerPath.rLineTo(-(25f.plus(7.5f / 2)) * sqrt(2f), (25f.plus(7.5f / 2)) * sqrt(2f))
+        centerPath.rLineTo(-radius * 2f, -radius * 2f)
+        centerPath.close()
+        shadowPath.op(centerPath, Path.Op.UNION)
+
+        val clockFacePath = Path()
+        clockFacePath.addCircle(center.x, center.y, radius, Path.Direction.CW)
+        shadowPath.op(clockFacePath, Path.Op.INTERSECT)
+
+        canvas.drawPath(shadowPath, shadowPaint)
+
     }
 
-    fun getTextBounds(): Rect {
-        getClockTextPaint().getTextBounds(allOnString, 0, 8, textBounds)
-        return textBounds
+    fun drawClock(canvas: Canvas) {
+
+        val center = getClockCenter()
+        val radius = getClockRadius()
+
+        val batteryPaint = Paint()
+        batteryPaint.shader = SweepGradient(center.x, center.y, Color.RED, Color.GREEN).apply {
+            val rotationMatrix = Matrix()
+            rotationMatrix.preRotate(-90f, center.x, center.y)
+            setLocalMatrix(rotationMatrix)
+        }
+        batteryPaint.style = Paint.Style.STROKE
+        batteryPaint.strokeWidth = 20f
+
+        canvas.drawArc(
+            RectF(
+                center.x - radius, center.y - radius, center.x + radius, center.y + radius
+            ),
+            -90f,
+            (batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY) * 3.6f),
+            false,
+            batteryPaint
+        )
+
+        val bodyPaint = Paint()
+        bodyPaint.color = backgroundImageUtils.getBaseColor()
+        bodyPaint.style = Paint.Style.FILL
+
+        // Main face
+        canvas.drawCircle(center.x, center.y, radius.minus(7.5f), bodyPaint)
+
+        bodyPaint.style = Paint.Style.STROKE
+        bodyPaint.strokeWidth = 10f
+        // Border outline
+        canvas.drawCircle(center.x, center.y, radius.plus(7.5f), bodyPaint)
+
+        val now = Calendar.getInstance()
+        val contrastColor = backgroundImageUtils.getColorOnBaseColor()
+
+        if (!keyguardManager.isKeyguardLocked) {
+            val currentTimeText =
+                (if (now.get(Calendar.SECOND) % 2 == 0) evenSimpleDateFormat else oddSimpleDateFormat).format(
+                    now
+                )
+            textPaint.color = backgroundImageUtils.getDarkerBaseColor()
+            canvas.drawRect(
+                RectF(
+                    center.x - (textBounds.width().toFloat() / 2f) - 20f,
+                    center.y - (radius / 2f) - (textBounds.height().toFloat()) + 10f,
+                    center.x + (textBounds.width().toFloat() / 2f) + 17.5f,
+                    center.y - (radius / 2f) + 47.5f
+                ), textPaint
+            )
+            textPaint.color = backgroundImageUtils.getDarkBaseColor()
+            canvas.drawRect(
+                RectF(
+                    center.x - (textBounds.width().toFloat() / 2f) - 10f,
+                    center.y - (radius / 2f) - (textBounds.height().toFloat()) + 20f,
+                    center.x + (textBounds.width().toFloat() / 2f) + 10f,
+                    center.y - (radius / 2f) + 40f
+                ), textPaint
+            )
+            textPaint.color = Color.argb(96, 128, 128, 128)
+            canvas.drawText(
+                allOnString,
+                center.x - (textBounds.width().toFloat() / 2f),
+                center.y - (radius / 2f) + 30f,
+                textPaint
+            )
+            textPaint.color = contrastColor
+            canvas.drawText(
+                currentTimeText,
+                center.x - (textBounds.width().toFloat() / 2f),
+                center.y - (radius / 2f) + 30f,
+                textPaint
+            )
+        }
+
+        val clockPaint = Paint()
+        clockPaint.color = contrastColor
+        clockPaint.style = Paint.Style.FILL
+
+        // Hours hand
+        drawHand(
+            canvas, center, 15f, radius.times(0.65f), Calendar.HOUR, clockPaint, radius.minus(7.5f)
+        )
+
+        // Minutes hand
+        drawHand(
+            canvas, center, 10f, radius.minus(7.5f), Calendar.MINUTE, clockPaint, radius.minus(7.5f)
+        )
+
+        // Seconds hand
+        drawHand(
+            canvas, center, 4f, radius.minus(7.5f), Calendar.SECOND, clockPaint, radius.minus(7.5f)
+        )
+
+        // Center piece
+        bodyPaint.style = Paint.Style.FILL
+        canvas.drawCircle(center.x, center.y, 25f, bodyPaint)
+        clockPaint.style = Paint.Style.STROKE
+        clockPaint.strokeWidth = 7.5f
+        canvas.drawCircle(center.x, center.y, 25f, clockPaint)
     }
 
-    fun getClockShadowPaint(): Paint {
-        return clockShadowPaint
+    fun handleTouchEvent(x: Int, y: Int) {
+        if (((getClockCenter().x - x.toFloat()).pow(2) + (getClockCenter().y - y.toFloat()).pow(2)) < (getClockRadius()
+                .pow(2))
+        ) {
+            val openAlarmIntent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
+            openAlarmIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            applicationContext.startActivity(openAlarmIntent)
+        }
     }
 
 }
