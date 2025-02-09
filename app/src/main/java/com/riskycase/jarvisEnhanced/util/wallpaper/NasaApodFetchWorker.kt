9e8aaa5d3f173be7d0f0e4eb9e4e68618081420b
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2025-02-09 20:31:54 +0530

    Minor cleanups

--- app/src/main/java/com/riskycase/jarvisEnhanced/util/wallpaper/NasaApodFetchWorker.kt
+++ app/src/main/java/com/riskycase/jarvisEnhanced/util/wallpaper/NasaApodFetchWorker.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Inject
 
 @HiltWorker
 class NasaApodFetchWorker @AssistedInject constructor(
